5c57c953c386ef138a427215271a5416847ba3ca
yuranym <yurany_sanchez69222@elpoli.edu.co>
2023-10-07 14:58:38 -0500

    Creacion de registro trabajadores

---
+++ Parcial1_YuranyZura_oct7/src/Finca/Cls_trabajadores.java
@@ -0,0 +1,103 @@
+
+package Finca;
+
+public class Cls_trabajadores {
+    private String str_nombres_completos;
+    private String str_apellidos;
+    private String str_id;
+    private String str_contacto;
+    private String str_correo;
+    private String str_direccion;
+    private String str_sexo;
+    private String str_salario;
+    
+    public Cls_trabajadores(String str_nombres, String apellidos, String id, String contacto, String correo, String direccion, String sexo, String salario){
+        this.str_nombres_completos = "";
+        this.str_apellidos = "";
+        this.str_id = "";
+        this.str_contacto = "";
+        this.str_correo = "";
+        this.str_direccion = "";
+        this.str_sexo = "";
+        this.str_salario = "";
+    }    
+
+    public String getStr_nombres_completos() {
+        return str_nombres_completos;
+    }
+
+    public String getStr_apellidos() {
+        return str_apellidos;
+    }
+
+    public String getStr_id() {
+        return str_id;
+    }
+
+    public String getStr_contacto() {
+        return str_contacto;
+    }
+
+    public String getStr_correo() {
+        return str_correo;
+    }
+
+    public String getStr_direccion() {
+        return str_direccion;
+    }
+
+    public String getStr_sexo() {
+        return str_sexo;
+    }
+
+    public String getStr_salario() {
+        return str_salario;
+    }
+
+    public void setStr_nombres_completos(String str_nombres_completos) {
+        this.str_nombres_completos = str_nombres_completos;
+    }
+
+    public void setStr_apellidos(String str_apellidos) {
+        this.str_apellidos = str_apellidos;
+    }
+
+    public void setStr_id(String str_id) {
+        this.str_id = str_id;
+    }
+
+    public void setStr_contacto(String str_contacto) {
+        this.str_contacto = str_contacto;
+    }
+
+    public void setStr_correo(String str_correo) {
+        this.str_correo = str_correo;
+    }
+
+    public void setStr_direccion(String str_direccion) {
+        this.str_direccion = str_direccion;
+    }
+
+    public void setStr_sexo(String str_sexo) {
+        this.str_sexo = str_sexo;
+    }
+
+    public void setStr_salario(String str_salario) {
+        this.str_salario = str_salario;
+    } 
+}
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+   
